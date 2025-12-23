c635bcf6735d1f161ea2aea383e8eefbac426157
Apurva <apurvapatil1334@gmail.com>
2025-12-23 23:56:29 +0530

    Defined API Paths in apiPath.js

---
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -0,0 +1,21 @@
+export const BASE_URL = "https://localhost:8000";
+
+
+// utils/apiPaths.js
+export const API_PATHS = {
+    AUTH : {
+        LOGIN:"/api/v1/auth/login",
+        REGISTER:"/api/v1/auth/register",
+        GET_USER_INFO:"/api/v1/auth/getUser",
+    },
+    Dashboard:{
+        GET_DATA:"/api/v1/dashboard",
+    },
+    INCOME:{
+        ADD_INCOME:"/api/v1/income/add",
+        GET_INCOME:"/api/v1/income/get",
+        DELETE_INCOME:(incomeId) => `/api/v1/income/${incomeId}`,
+        DOWNLOAD_INCOME:"/api/v1/income/downloadexcel",
+    }
+   
+}    
